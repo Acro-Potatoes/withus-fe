497c82d0d78a168456e5c99520a4b763a2072c1e
KKamjangE <ajm980718@gmail.com>
2024-02-16 11:28:26 +0900

    Style: 코드 포멧팅

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
         <main className="flex justify-center h-dvh">
             <h2 className="text-4xl self-center">메인 페이지</h2>
         </main>
-    );
+    )
 }
