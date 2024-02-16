74fc4a2bbe3846a855564bdd9c25bd5395f2eb56
KKamjangE <ajm980718@gmail.com>
2024-02-16 15:48:24 +0900

    Feat: 공통 Input 컴포넌트 구현

---
+++ app/ui/inputs.tsx
@@ -0,0 +1,13 @@
+import { InputHTMLAttributes } from 'react'
+
+export const TextField = ({
+    ...rest
+}: InputHTMLAttributes<HTMLInputElement>) => {
+    return (
+        <input
+            className="peer w-full border border-black rounded-md py-2 px-4 text-md placeholder:text-gray-400 focus:outline-orange-400 my-2"
+            {...rest}
+            autoComplete="off"
+        />
+    )
+}
