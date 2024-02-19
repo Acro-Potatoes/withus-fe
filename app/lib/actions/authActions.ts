08f441f695f740f9b0551e2113a1e581719674ad
KKamjangE <ajm980718@gmail.com>
2024-02-19 18:01:41 +0900

    Rename: action 파일 분리

--- app/lib/actions/authActions.ts
+++ app/lib/actions/authActions.ts
@@ -1,10 +1,13 @@
 'use server'
 
+import { z } from 'zod'
+
 export const signIn = async (formData: FormData) => {
     const rawFormData = {
         email: formData.get('email'),
         password: formData.get('password'),
     }
+
     console.log(rawFormData)
 }
 
@@ -12,6 +15,9 @@ export const signUp = async (formData: FormData) => {
     const rowFormData = {
         email: formData.get('email'),
         password: formData.get('password'),
+        confirmPassword: formData.get('confirmPassword'),
+        userName: formData.get('userName'),
+        phone: formData.get('phone'),
     }
     console.log(rowFormData)
 }
