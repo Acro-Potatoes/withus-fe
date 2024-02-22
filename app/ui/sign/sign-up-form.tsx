c7f2acf15b6b17256c8c9bb097000718754ad7ce
KKamjangE <ajm980718@gmail.com>
2024-02-22 14:02:04 +0900

    Feat: inpuit 컴포넌트 변경

--- app/ui/sign/sign-up-form.tsx
+++ app/ui/sign/sign-up-form.tsx
@@ -23,57 +23,62 @@ const SignUpForm = () => {
         <form action={signUp} className="flex flex-col gap-2">
             <div>
                 <label htmlFor="email">이메일</label>
-                <TextField
+                <FormField
                     id="email"
-                    name="email"
                     type="email"
+                    name="email"
                     placeholder="name@work-email.com"
                     required
                     autoFocus
+                    register={register}
+                    error={errors.email}
                 />
             </div>
             <div>
                 <label htmlFor="password">비밀번호</label>
-                <TextField
+                <FormField
                     id="password"
                     name="password"
                     type="password"
                     placeholder="password"
                     required
+                    register={register}
+                    error={errors.password}
                 />
             </div>
             <div>
                 <label htmlFor="confirmPassword">비밀번호 확인</label>
-                <TextField
+                <FormField
                     id="confirmPassword"
                     name="confirmPassword"
                     type="password"
                     placeholder="password"
                     required
+                    register={register}
+                    error={errors.confirmPassword}
                 />
             </div>
             <div>
                 <label htmlFor="userName">이름</label>
-                <TextField
+                <FormField
                     id="userName"
                     name="userName"
                     placeholder="이름을 입력해주세요."
+                    required
+                    register={register}
+                    error={errors.userName}
                 />
             </div>
             <div>
                 <label htmlFor="phone">전화번호</label>
-                <TextField
+                <FormField
                     id="phone"
                     name="phone"
                     placeholder="'-' 를 제외하고 입력해주세요."
+                    register={register}
+                    error={errors.phone}
                 />
             </div>
-            <FormField
-                type="email"
-                name="email"
-                register={register}
-                error={errors.email}
-            />
             <Button type="submit">계속</Button>
         </form>
     )
